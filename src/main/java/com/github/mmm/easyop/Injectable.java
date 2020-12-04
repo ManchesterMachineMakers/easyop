64382e0dd979913d09af9024f4ba186bfe20a960
munchkinhalfling <munchkinhalfling@pm.me>
2020-12-04 13:22:47 -0500

    Add support for alternate injectors

--- src/main/java/com/github/mmm/easyop/Injectable.java
+++ src/main/java/com/github/mmm/easyop/Injectable.java
@@ -7,23 +7,27 @@ import java.util.Map;
 
 public interface Injectable {
     void init(HashMap<String, ?> params) throws ReflectiveOperationException;
-    public static HashMap<Class<?>, Injectable> globals = new HashMap<>();
-    public static <T, I extends Injectable> void injectAll(T obj, Class<I> tInject) throws ReflectiveOperationException {
+    HashMap<Class<?>, Injectable> globals = new HashMap<>();
+    static <T, I extends Injectable> void injectAll(T obj, Class<I> tInject) throws ReflectiveOperationException {
         injectAll(obj, tInject, new HashMap<>());
     }
-    public static <T, I extends Injectable> void injectAll(T obj, Class<I> tInject, HashMap<String, ?> params) throws ReflectiveOperationException {
+    static <T, I extends Injectable> void injectAll(T obj, Class<I> tInject, HashMap<String, ?> params) throws ReflectiveOperationException {
         Class<T> cls = (Class<T>) obj.getClass();
         Field[] fields = cls.getDeclaredFields();
         for(Field field : fields) {
             Annotation[] annotations = field.getDeclaredAnnotations();
             for (Annotation anno : annotations) {
-                if(anno.annotationType() == Inject.class && tInject.isAssignableFrom(field.getType())) {
-                    if(!globals.containsKey(field.getType())) {
-                        Injectable instance = (Injectable) field.getType().newInstance();
-                        instance.init(params);
-                        globals.put(field.getType(), instance);
+                if(anno.annotationType() == Inject.class) {
+                    Class<?> tField = ((Inject)anno).value();
+                    if(tField == Object.class) tField = field.getType();
+                    if(tInject.isAssignableFrom(tField)) {
+                        if (!globals.containsKey(tField)) {
+                            Injectable instance = (Injectable) tField.newInstance();
+                            instance.init(params);
+                            globals.put(field.getType(), instance);
+                        }
+                        field.set(obj, field.getType().cast(globals.get(field.getType())));
                     }
-                    field.set(obj, field.getType().cast(globals.get(field.getType())));
                 }
             }
         }
