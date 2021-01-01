e5d3592179a01533fae348588dfbe04daf8e2ecf
munchkinhalfling <munchkinhalfling@pm.me>
2021-01-01 13:57:33 -0500

    Use opmode name for logging tag if possible

--- src/main/java/com/github/mmm/easyop/util/logging/Logger.java
+++ src/main/java/com/github/mmm/easyop/util/logging/Logger.java
@@ -1,5 +1,8 @@
 package com.github.mmm.easyop.util.logging;
 import android.util.Log;
+import java.lang.annotation.Annotation;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 public interface Logger {
 	default void log(String message) {
@@ -7,7 +10,16 @@ public interface Logger {
 	}
 	default void log(LogPriority priority, String message) {
 		Class<?> cls = this.getClass();
+		Annotation[] annotations = cls.getDeclaredAnnotations();
 		String name = cls.getName();
+		for(Annotation anno : annotations) {
+			if(anno.annotationType() == Autonomous.class) {
+				name = ((Autonomous)anno).name();
+				break;
+			} else if(anno.annotationType() == TeleOp.class) {
+				name = ((TeleOp)anno).name();
+			}
+		}
 		int realPriority = Log.INFO;
 		switch(priority) {
 			case Assert:
