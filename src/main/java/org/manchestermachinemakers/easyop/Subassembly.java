8a2ada0d14e080172e40b95028936ae6c580f8f5
Aleks Rūtiņš <aleks@rutins.com>
2023-11-04 14:41:49 +0000

    Add customInit()

--- src/main/java/org/manchestermachinemakers/easyop/Subassembly.java
+++ src/main/java/org/manchestermachinemakers/easyop/Subassembly.java
@@ -4,9 +4,13 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import java.util.HashMap;
 
 public interface Subassembly extends Base, Injectable {
+    default void customInit(HardwareMap hardwareMap) {
+
+    }
     default void init(HashMap<String, ?> args) throws ReflectiveOperationException {
         HardwareMap hwMap = (HardwareMap)args.get("hwMap");
         devices(hwMap);
         subassemblies(hwMap);
+        customInit(hwMap);
     }
 }
